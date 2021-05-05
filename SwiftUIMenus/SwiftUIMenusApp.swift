46637a6bc225e3752ba768008c9763a77eb9be09
zamong25 <wool0602@naver.com>
2021-05-05 19:50:05 +0900

    Initial Commit

---
+++ SwiftUIMenus/SwiftUIMenusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMenusApp.swift
+//  SwiftUIMenus
+//
+//  Created by PCW on 2021/05/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMenusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
